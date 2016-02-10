f990b9627a15979514e9b359886243d3686558f2
FuRa1 <lalique.surf@gmail.ru>
2016-02-10 21:18:57 +0200

    22

--- js/page.js
+++ js/page.js
@@ -10,19 +10,22 @@ $(function () {
         this.link = "http://www.x-bikers.ru/bikera/respect.php?id=" + this.id + "&act=1";
     }
 
-    var idArray = [{id:293673}, {id:298300}, {id:323788}, {id:291056}, {id:308629}, {id:311156}, {id:308546}, {id:318206}];
+    var idArray = [{id: 293673}, {id: 298300}, {id: 323788}, {id: 291056}, {id: 308629}, {id: 311156}, {id: 308546}, {id: 318206}];
 
-    function redirect(index){
+    function redirect(index) {
+
+        var act = "&act=1";
+
         setTimeout(function () {
 
-            console.log("http://www.x-bikers.ru/bikera/respect.php?id=" + idArray[index].id + "&act=1");
-        window.open("http://www.x-bikers.ru/bikera/respect.php?id=" + idArray[index].id + "&act=1");
+
+            window.open("http://www.x-bikers.ru/bikera/respect.php?id=" + idArray[index].id);
         }, 3000 * index)
     }
 
     function plus() {
         for (var i = 0; i < idArray.length; i++) {
-           redirect(i);
+            redirect(i);
         }
     }
 
