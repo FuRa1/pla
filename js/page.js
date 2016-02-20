600e675fc2e301567fbee4b02f8eea5ec1d4da7c
FuRa1 <lalique.surf@gmail.ru>
2016-02-20 10:35:45 +0200

    'msg'

--- js/page.js
+++ js/page.js
@@ -1,3 +1,4 @@
+'use strict';
 $(function () {
 
     var $button = $('#myButton');
@@ -8,18 +9,14 @@ $(function () {
 
     var idArray = [{id: 293673}, {id: 298300}, {id: 323788}, {id: 291056}, {id: 308629}, {id: 311156}, {id: 308546}, {id: 318206}, {id:332758}, {id:318902}];
 
-    function redirect(index) {
-
-        var url = "http://www.x-bikers.ru/bikera/respect.php?id=" + idArray[index].id + "&act=1";
-
-        setTimeout(function () {
-            $frame.attr('src',url);
-        }, 5000 * index)
-    }
 
     function plus() {
-        for (var i = 0; i < idArray.length; i++) {
-            redirect(i);
+        for (let i = 0; i < idArray.length; i++) {
+
+            setTimeout(function () {
+                var url = "http://www.x-bikers.ru/bikera/respect.php?id=" + idArray[i].id + "&act=1";
+                $frame.attr('src',url);
+            }, 5000 * i)
         }
     }
 
